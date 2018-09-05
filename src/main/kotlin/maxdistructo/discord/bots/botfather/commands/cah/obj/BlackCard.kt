82913fda9a153d32dd614afc2bf981b792ff42db
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-09-05 15:33:22 -0500

    Create BlackCard.kt

---
+++ src/main/kotlin/maxdistructo/discord/bots/botfather/commands/cah/obj/BlackCard.kt
@@ -0,0 +1,19 @@
+package maxdistructo.discord.bots.botfather.commands.cah.obj
+
+class BlackCard : ICAHCard{
+
+override val color = CardColor.BLACK
+override val text : String
+override var blankValues : List<String>
+
+constructor(textIn : String, vararg values : String){
+  text = textIn
+  blankValues = values
+} 
+
+constructor(textIn : String){
+  text = textIn
+  blankValues = listOf() //This will be used more often but if a card needs to be copied, the other method would be used.
+}
+
+}
